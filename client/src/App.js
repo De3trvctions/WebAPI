b68503db499f7a8c14b23522973f1e8c063e66b1
Kelden <keldenong@gmail.com>
2019-11-22 00:11:23 +0800

    Update Commit 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,121 +1,294 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-import Popup from 'react-popup';
-import './Popup.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import GameCard from "./GameCard";
 import {
-  Jumbotron,
-  Alert,
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Modal,
-  ModalHeader,
-  ModalBody
+    Jumbotron,
+    Alert,
+    Container,
+    Row,
+    Col,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Modal,
+    ModalHeader,
+    ModalBody
 } from 'reactstrap';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: []
+    constructor() {
+        super();
+        this.state = {
+            alertVisible: false,
+            title: '',
+            movies: [{ background_image: '', name: 'hello', id: 1 }],
+            modal: false,
+            currentPage: 1,
+            todosPerPage: 3
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onFindGame = this.onFindGame.bind(this);
+        this.onDismiss = this.onDismiss.bind(this);
+        this.toggle = this.toggle.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    //for popup
+    onDismiss() {
+        this.setState({ alertVisible: false });
+    }
+
+    //for form
+    onSubmit = e => {
+        e.preventDefault();
+        this.setState({ alertVisible: false });
+
+        const query = `/create?name=${this.state.title}`;
+
+        axios
+            .get(query)
+            .then(result => {
+                console.log(result.data);
+                if (result.data === 'Not found') {
+                    this.setState({ alertVisible: true });
+                } else if (result.data === 'This game already exist in the database') {
+                    alert(result.data);
+                }
+                this.getAllMovies();
+            })
+            .catch(error => {
+                alert('Error: ', error);
+            });
+        this.toggle();
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  getAllMovies = () => {
-    axios
-      .get('/getallmovies')
-      .then(result => {
-        this.setState({ movies: result.data });
-        console.log(this.state.movies);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-  componentDidMount() {
-    this.getAllMovies();
-  }
-
-  handleSubmit(e) {
-    const query = `/getmovie?title=${this.input.value}`;
-
-    console.log(query);
-    e.preventDefault();
-    axios
-      .get(query)
-      .then(result => {
-        console.log(result);
-        if (result.data === 'Not found') {
-          Popup.alert('Movie Not Found');
+    onFindGame = e => {
+        e.preventDefault();
+        this.setState({ alertVisible: false });
+        if (e.target.value == "") {
+            this.getAllMovies();
+        } else {
+            const query = `/findgame?name=${e.target.value}`;
+            axios.get(query).
+                then(result => {
+                    this.setState({ movies: result.data });
+                    console.log(result.data);
+                }).catch(err => {
+                    console.log(err);
+                });
         }
+    }
+
+    // for form field
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+
+    }
+
+    getAllMovies = () => {
+        axios
+            .get("/getAllRecord")
+            .then(result => {
+                this.setState({ movies: result.data });
+                console.log(this.state.movies);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    componentDidMount() {
         this.getAllMovies();
-      })
-      .catch(error => {
-        alert('Error: ', error);
-      });
-  }
+    }
 
-  deleteRecord = value => {
-    console.log('to delete: ', value);
-    const query = `/deletemovie?title=${value}`;
-    axios
-      .get(query)
-      .then(result => {
-        this.getAllMovies();
-      })
-      .catch(error => {
-        alert('Error: ', error);
-      });
-  };
+    deleteRecord = value => {
+        console.log("to delete: ", value);
+        const query = `/delete?id=${value}`;
+        axios
+            .get(query)
+            .then(result => {
+                this.getAllMovies();
+            })
+            .catch(error => {
+                alert("Error: ", error);
+            });
+    };
 
-  //https://www.codementor.io/blizzerand/building-forms-using-react-everything-you-need-to-know-iz3eyoq4y
-  //todo add buttons to delete rows
-  //https://codepen.io/aaronschwartz/pen/awOyQq?editors=0010
-  //https://github.com/react-tools/react-table/issues/324
-  render() {
-    var data = this.state.movies;
-    data = data.reverse();
+    editRecord = (id, value, platform, genres) => {
 
-    return (
-      <div className="App">
-        <Container>
-          <Jumbotron>
-            <h1 className="display-3">Game Search</h1>
-            <p className="lead">Search for game</p>
-          </Jumbotron>
-          <Row>
-            <Col>
-              <Alert
-                color="danger"
-                isOpen={this.state.alertVisible}
-                toggle={this.onDismiss}
-              >
-              Game not found
-              </Alert>
-            </Col>
-          </Row>
-          <Row>
-            <Col sm="9">
-              <Input
-                type="text"
-                name="title"
-                id="findGame"
-                placeholder="Search a game title..."
-                onChange={this.onFindGame}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+        const query = `/update?id=${id}&text=${value}&platform=${platform}&genres=${genres}`;
+        console.log(query);
+        axios
+            .get(query)
+            .then(result => {
+                this.getAllMovies();
+                console.log("updated");
+            }).catch(error => {
+
+                alert("Error:" + error);
+            });
+
+    };
+
+    handleClick(event) {
+        this.setState({
+            currentPage: Number(event.target.id)
+        });
+    }
+
+    //https://www.codementor.io/blizzerand/building-forms-using-react-everything-you-need-to-know-iz3eyoq4y
+    //todo add buttons to delete rows
+    //https://codepen.io/aaronschwartz/pen/awOyQq?editors=0010
+    //https://github.com/react-tools/react-table/issues/324
+    render() {
+
+        const { movies, currentPage, todosPerPage } = this.state;
+
+        // Logic for displaying todos
+        const indexOfLastTodo = currentPage * todosPerPage;
+        const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+        const currentTodos = movies.slice(indexOfFirstTodo, indexOfLastTodo);
+
+        const renderTodos = currentTodos.map((movies, index) => {
+            return (
+                <Col xs="4">
+                    <GameCard
+                        removeGame={this.deleteRecord.bind(this)}
+                        editGame={this.editRecord.bind(this)}
+                        games={movies}
+                    />
+                </Col>
+
+            );
+        });
+
+        // Logic for displaying page numbers
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(movies.length / todosPerPage); i++) {
+            pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+                <li
+                    key={number}
+                    id={number}
+                    onClick={this.handleClick}
+                >
+                    {number}
+                </li>
+            );
+        });
+
+        function HasGame(){
+            if(movies.length == 0){
+                
+                return(
+                    
+                    <div class="noGameDiv">
+
+                        <img src="http://pm1.narvii.com/6688/d91c5f2917fe35c3862d2b2a8a2fc91c89e16450_00.jpg">
+                            
+                        </img>
+                        <p>No result is found</p>
+                    </div>
+                
+                );
+            }
+
+            return(<br></br>);
+        }
+
+        return (
+            <div className="App">
+
+                <Container>
+                    <Jumbotron>
+                        <h1 className="display-4">Game Search</h1>
+                        <p className="lead">Search for game</p>
+                    </Jumbotron>
+                    <Row>
+                        <Col>
+                            <Alert
+                                color="danger"
+                                isOpen={this.state.alertVisible}
+                                toggle={this.onDismiss}
+                            >
+                                Game not found
+                            </Alert>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="9">
+                            <Input
+                                type="text"
+                                name="title"
+                                id="findGame"
+                                placeholder="Search a game title..."
+                                onChange={this.onFindGame}
+                            />
+                        </Col>
+
+                        <Col sm="3">
+                            <div>
+                                <Button color="success" onClick={this.toggle}>Request a game</Button>
+                                <Modal isOpen={this.state.modal} toggle={this.toggle} className="modaltest">
+                                    <ModalHeader toggle={this.toggle}>Request a game</ModalHeader>
+                                    <ModalBody>
+
+                                        <Form onSubmit={this.onSubmit}>
+                                            <FormGroup>
+                                                <Label for="title">Add a Game</Label>
+                                                <Input
+                                                    type="text"
+                                                    name="title"
+                                                    id="title"
+                                                    placeholder="enter game title..."
+                                                    onChange={this.onChange}
+                                                />
+                                            </FormGroup>
+                                            <Button color="primary">Submit</Button>
+                                        </Form>
+
+                                    </ModalBody>
+                                </Modal>
+                            </div>
+
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <HasGame></HasGame>
+                        <div class="page-items">
+                            <ul>
+                                <Row>
+                                    {renderTodos}
+                                </Row>
+
+                            </ul>
+                            <ul id="page-numbers">
+                                {renderPageNumbers}
+                            </ul>
+                        </div>
+                    </Row>
+
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/GameCard.js
@@ -0,0 +1,209 @@
+import React, { Component } from 'react';
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button,
+    Row,
+    Col
+} from 'reactstrap';
+
+import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
+
+import { testFunction, editText, mousein, mouseout } from './control'
+
+export class GameCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+            nested: false,
+            close: false
+        };
+        this.toggle = this.toggle.bind(this);
+        this.toggleNested = this.toggleNested.bind(this);
+        this.toggleAll = this.toggleAll.bind(this);
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    toggleNested = () => {
+        this.setState({
+            nested: !this.state.nested,
+            close: false
+        });
+    }
+
+    toggleAll = () => {
+        this.setState({
+            nested: !this.state.nested,
+            close: true
+        });
+    }
+
+    delete = () => {
+        this.setState({
+            nested: !this.state.nested,
+            close: false
+        });
+
+        this.props.removeGame(this.props.games.id);
+    }
+
+    edit = () => {
+
+        console.log(this.props);
+        var resultArray = testFunction();
+        console.log(resultArray);
+        this.props.editGame(this.props.games.id, resultArray[0], resultArray[1], resultArray[2]);
+    }
+
+    openEdit = () => {
+
+        editText();
+    }
+
+    handleHover = () => {
+        mousein(this.props.games.id);
+    }    
+    
+    handleHoverOut = () => {
+        mouseout(this.props.games.id);
+    }
+
+
+    render() {
+
+        function PlatformList(props) {
+            const numbers = props.platform;
+            const listItems = numbers.map((number) =>
+                <li>{number}</li>
+            );
+            return (
+                <ul>{listItems}</ul>
+            );
+        }
+
+        function TimeStamp(props) {
+
+            // Months array
+            var months_arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            // Convert timestamp to milliseconds
+            var date = new Date(props.timeStamp * 1000);
+            // Year
+            var year = date.getFullYear();
+            // Month
+            var month = months_arr[date.getMonth()];
+            // Day
+            var day = date.getDate();
+
+            // Display date time in MM-dd-yyyy format
+            var formattedTime = day + '-' + month + '-' + year;
+
+            return (
+                <p>{formattedTime}</p>
+            );
+        }
+
+        function GenresList(props) {
+            const numbers = props.genres;
+            const listItems = numbers.map((number) =>
+                <li>{number}</li>
+            );
+            return (
+                <ul>{listItems}</ul>
+            );
+        }
+
+        return (
+
+            <div>
+                <Card onMouseOver={this.handleHover} onMouseOut={this.handleHoverOut} id={"a"+this.props.games.id}>
+                    <CardImg class="cardImage" onClick={this.toggle} top width="20%" src={this.props.games.background_image} alt="Card image cap" />
+                    <p class="cardtitle"> {this.props.games.name}</p>
+                    <div>
+                        <Modal isOpen={this.state.modal} toggle={this.toggle} className="modaltest">
+                            <ModalHeader toggle={this.toggle}>{this.props.games.name}</ModalHeader>
+
+                            <ModalBody>
+
+                                <Row>
+
+                                    <Col>
+                                        <img src={this.props.games.background_image} width="100%" />
+                                        <br />
+                                        <Button color="danger" onClick={this.toggleNested}>Delete</Button>
+                                        <Button color="primary" onClick={this.openEdit}>Edit</Button>
+                                        <br />
+                                    </Col>
+
+                                    <Col>
+                                        <h5>Summary</h5>
+                                        <textarea id="textarea" disabled  >{this.props.games.summary}</textarea>
+                                        <br />
+
+                                        <div>
+                                            <h5>Platform</h5>
+                                            <div id="platformDiv">
+                                                <PlatformList platform={this.props.games.platform} />
+                                            </div>
+                                            <div id="editPlatformDiv">
+                                                <input id="platformInput" type="text" name="plattform" defaultValue={this.props.games.platform}></input>
+                                            </div>
+
+                                        </div>
+
+                                        <h5>Release Date</h5>
+                                        <TimeStamp timeStamp={this.props.games.first_release_date} />
+
+                                        <div>
+                                            <h5>Genres</h5>
+                                            <div id="genresDiv">
+                                                <GenresList genres={this.props.games.genres} />
+                                            </div>
+                                            <div id="editGenresDiv">
+                                                <input id="genresInput" type="text" name="genres" defaultValue={this.props.games.genres}></input>
+                                            </div>
+                                        </div>
+
+                                        <Modal isOpen={this.state.nested} toggle={this.toggleNested} onClosed={this.state.close ? this.toggle : undefined}>
+                                            <ModalHeader>Delete</ModalHeader>
+                                            <ModalBody>Do you want to delete this?</ModalBody>
+                                            <ModalFooter>
+                                                <Button color="primary" onClick={this.delete}>Done</Button>{' '}
+                                            </ModalFooter>
+                                        </Modal>
+                                    </Col>
+
+                                </Row>
+
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button id="saveBtn" color="success" onClick={this.edit}>Save</Button>{' '}
+                                <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                            </ModalFooter>
+                        </Modal>
+                    </div>
+                </Card>
+            </div>
+
+        );
+    }
+}
+
+export default GameCard;
+
+
+
